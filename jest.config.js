d29ef00784ac97f6a2667bcfdaa1f66c42837a41
Sheikh M. Salahuddin <smsalah@gmail.com>
2023-07-18 21:00:20 +0500

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+
+const setupFilesAfterEnv = jestConfig.setupFilesAfterEnv || [];
+setupFilesAfterEnv.push('<rootDir>/jest-sa11y-setup.js');
+
+module.exports = {
+    ...jestConfig,
+    moduleNameMapper: {
+        '^lightning/navigation$':
+            '<rootDir>/force-app/test/jest-mocks/lightning/navigation',
+        '^lightning/platformShowToastEvent$':
+            '<rootDir>/force-app/test/jest-mocks/lightning/platformShowToastEvent',
+        '^lightning/messageService$':
+            '<rootDir>/force-app/test/jest-mocks/lightning/messageService',
+        '^lightning/mobileCapabilities$':
+            '<rootDir>/force-app/test/jest-mocks/lightning/mobileCapabilities'
+    },
+    setupFilesAfterEnv
+};
